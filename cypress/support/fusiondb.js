ffa6369ed030dd26eefd8f27fb8ab7aac07dff38
Charafeddine Cheraa <ccheraa@gmail.com>
2020-08-11 17:38:24 +0100

    added option to filter tests by api version

--- cypress/support/fusiondb.js
+++ cypress/support/fusiondb.js
@@ -7,5 +7,54 @@ Cypress.fusionDB = {
         return;
       }
     }
-  }
+  },
+  minApi(minVersion, callback, strict) {
+    const tags = Cypress.env('TEST_TAGS').split(',');
+    const versionTag = tags.find(tag => tag.substr(0, 3) === 'API');
+    if (!versionTag && !strict) {
+      callback();
+    } else {
+      const [minMajor, minMinor, minPatch] = minVersion.split('.').map(val => Number.parseInt(val));;
+      const [major, minor, patch] = versionTag.substr(3).split('.').map(val => Number.parseInt(val));
+      if ((major > minMajor)
+        || (major === minMajor && minor > minMinor)
+        || (minor === minMinor && patch >= minPatch)) {
+        callback();
+      }
+    }
+  },
+  maxApi(maxVersion, callback, strict) {
+    const tags = Cypress.env('TEST_TAGS').split(',');
+    const versionTag = tags.find(tag => tag.substr(0, 3) === 'API');
+    if (!versionTag && !strict) {
+      callback();
+    } else {
+      const [maxMajor, maxMinor, maxPatch] = maxVersion.split('.').map(val => Number.parseInt(val));;
+      const [major, minor, patch] = versionTag.substr(3).split('.').map(val => Number.parseInt(val));
+      if ((major < maxMajor)
+        || (major === maxMajor && minor < maxMinor)
+        || (minor === maxMinor && patch < maxPatch)) {
+        callback();
+      }
+    }
+  },
+  api(minVersion, maxVersion, callback, strict) {
+    const tags = Cypress.env('TEST_TAGS').split(',');
+    const versionTag = tags.find(tag => tag.substr(0, 3) === 'API');
+    if (!versionTag && !strict) {
+      callback();
+    } else {
+      const [minMajor, minMinor, minPatch] = minVersion.split('.').map(val => Number.parseInt(val));;
+      const [maxMajor, maxMinor, maxPatch] = maxVersion.split('.').map(val => Number.parseInt(val));;
+      const [major, minor, patch] = versionTag.substr(3).split('.').map(val => Number.parseInt(val));
+      if (((major < maxMajor)
+        || (major === maxMajor && minor < maxMinor)
+        || (minor === maxMinor && patch < maxPatch))
+        && ((major > minMajor)
+          || (major === minMajor && minor > minMinor)
+          || (minor === minMinor && patch >= minPatch))) {
+        callback();
+      }
+    }
+  },
 }
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -4,6 +4,8 @@ declare namespace Cypress {
   interface Cypress {
     fusionDB: {
       allow(allowedTags: string[], callback: () => any): void;
+      minApi(minVersion: string, callback: () => any, strict?: boolean): void;
+      maxApi(maxVersion: string, callback: () => any, strict?: boolean): void;
     }
   }
 }
