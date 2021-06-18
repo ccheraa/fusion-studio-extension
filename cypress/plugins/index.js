dd3ebb698e36f5ef9be43c20e4c489cba8efd43d
Charafeddine Cheraa <ccheraa@gmail.com>
2021-06-18 08:57:53 +0100

    added upload dialog test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  config.env.homedir = require('os').homedir();
+  return config;
 }
